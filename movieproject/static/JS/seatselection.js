43dd2b2c373ac2a2dfc9116b98b783a9f86a0f9c
Mahesh M <maheshmuralimvk@gmail.com>
2023-12-03 09:32:24 +0530

    Script Updated

--- movieproject/static/JS/seatselection.js
+++ movieproject/static/JS/seatselection.js
@@ -1,16 +1,22 @@
 let numberOfSeat=0;
+let id=[];  
 for (let index = 0; index < document.querySelectorAll(".chair").length; index++) {
     document.querySelectorAll("li")[index].addEventListener("click",function () {
         var chairList = this.classList;
-        var id = this.id;
         if(chairList.contains("chairselect")){
             chairList.remove("chairselect");
             numberOfSeat = numberOfSeat-1;
+            id.pop(this.id);
         }else{
             chairList.add("chairselect");
             numberOfSeat = numberOfSeat+1;
+            id.push(this.id);
         }
         document.querySelector("#amount").innerHTML="BookNow"+"("+numberOfSeat*250+")";
+        document.querySelector("#noOfSeat").innerHTML=numberOfSeat;
+        document.querySelector("#seat").innerHTML=id;
+        document.querySelector("#price").innerHTML=numberOfSeat*250;
+        console.log(id)
     });
     
 }
